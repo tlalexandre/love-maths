f7ff35d98f3ce66f828f4006b5de5e90ecc55070
BanditCook <tanguy.lalexandre@gmail.com>
2023-05-24 11:55:29 +0100

    Add an alert when you click the buttons

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,18 @@
+// Wait for the DOM to finish loading before running the game.
+// Get the button elements and add event listeners to them.
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByTagName("button");
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      if (this.getAttribute("data-type") === "submit") {
+        alert("You clicked the submit button");
+      } else {
+        let gameType = this.getAttribute("data-type");
+        alert("You clicked the " + gameType + " button");
+      }
+    });
+  }
+});
 function runGame() {}
 function checkAnswer() {}
 function calculateCorrectAnswer() {}
