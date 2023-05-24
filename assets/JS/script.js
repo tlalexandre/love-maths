6956a8b31226a3109c4a6f39ff0e738f670d4bb9
BanditCook <tanguy.lalexandre@gmail.com>
2023-05-24 11:43:35 +0100

    Set up structure of game functions

---
+++ assets/JS/script.js
@@ -0,0 +1,10 @@
+function runGame() {}
+function checkAnswer() {}
+function calculateCorrectAnswer() {}
+
+function incrementScore() {}
+function incrementWrongAnswer() {}
+function displayAdditionQuestion() {}
+function displaySubstractQuestion() {}
+function displayMultiplyQuestion() {}
+function displayDivideQuestion() {}
